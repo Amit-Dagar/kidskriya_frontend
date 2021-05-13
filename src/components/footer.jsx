13f9443f15a2fb7bf4d361ad93a4388c09ecf00e
Jay Aware <jay.aware.02@gmail.com>
2021-05-13 13:05:05 +0530

    Add Contact Us in Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,6 +7,9 @@ export default class Footer extends PureComponent {
       <footer className="bg-dark">
         <div className="pt-5 bg-darker">
             <div className="container">
+              <td
+                style={{display:"flex", justifyContent: "space-evenly" }}>
+              <tr>
               <div
                 className="widget w-100 mb-4 pb-3 text-center mx-auto"
                 style={{ maxWidth: "28rem" }}
@@ -56,6 +59,35 @@ export default class Footer extends PureComponent {
                   <div className="subscription-status"></div>
                 </form>
               </div>
+              </tr>
+              <tr>
+                <div className="widget">
+                  <div className="input-group">
+                    <i className="ci-phone text-light" style={{padding: ".5em"}}> </i>
+                    <h3 className="widget-title text-light pb-1">
+                        Contact Us
+                    </h3>
+                  </div>
+                  <hr />
+                  <div
+                    style={{display:"grid", padding:"1px"}}>
+                    <p className="form-text text-light opacity-50"
+                      style={{margin:"5px"}}>
+                      Phone Number: +91 XXXXX XXXXX
+                    </p>
+                    <p className="form-text text-light opacity-50"
+                      style={{margin:"5px"}}>
+                      E-Mail Address: <a href="mailto:email@example.com">email@example.com</a>
+                    </p>
+                    <p className="form-text text-light opacity-50"
+                      style={{margin:"5px"}}>
+                      Write To Us at Address_Address
+                    </p>
+                  </div>
+                  
+                </div>
+              </tr>
+              </td>
             <hr className="hr-light mb-3" />
             <div className="d-md-flex justify-content-between pt-4">
               <div className="pb-4 fs-xs text-light opacity-50 text-center text-md-start">
